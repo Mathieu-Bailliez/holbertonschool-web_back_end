30367a2f20b97074e117ca476ff56e4ec4ef7f66
Mathieu-Bailliez <12996@holbertonstudents.com>
2026-06-18 06:15:48 +0200

    feat: implement wait_random async coroutine

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -1,15 +1,12 @@
 #!/usr/bin/env python3
-"""Module définissant une coroutine d'attente aléatoire asynchrone."""
+"""Module defining an asynchronous random delay coroutine."""
 
 import asyncio
 import random
 
 
 async def wait_random(max_delay: int = 10) -> float:
-    """Attend de manière asynchrone un délai aléatoire entre 0 et
-    max_delay secondes (inclus), puis retourne ce délai sous forme
-    de nombre flottant.
-    """
+
     delay: float = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
     return delay
