13f7be6043b388d90df73988e649ad15c16b12dc
Mathieu-Bailliez <12996@holbertonstudents.com>
2026-06-19 18:55:24 +0200

    delete: double documentation

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -7,9 +7,8 @@ import random
 from typing import AsyncGenerator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> AsyncGenerator[float]:
     """Yield random numbers asynchronously."""
-    """Generate 10 random numbers asynchronously."""
 
     for _ in range(10):
         await asyncio.sleep(1)
