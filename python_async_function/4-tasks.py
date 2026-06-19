897d3fd294c0bb742d6d1261ef7fe83d2d77cc21
Mathieu-Bailliez <12996@holbertonstudents.com>
2026-06-19 14:28:55 +0200

    Execute multiple tasks concurrently and return their delays.

---
+++ python_async_function/4-tasks.py
@@ -0,0 +1,25 @@
+#/usr/bin/env python3
+"""Execute multiple tasks concurrently and return their delays."""
+
+
+import asyncio
+from typing import List
+
+
+task_wait_random = __import__("3-tasks").task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """Execute task_wait_random n times concurrently and return delays."""
+    results = []
+    tasks = []
+
+    for _ in range(n):
+        task = task_wait_random(max_delay)
+        task.append(tasks)
+
+    for completed in asyncio.as_completed(tasks):
+        delay = await completed
+
+    results.append(delay)
+    return results
