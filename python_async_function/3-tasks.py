aee3ec4959d39fa287a56db7402fca285cc94801
Mathieu-Bailliez <12996@holbertonstudents.com>
2026-06-19 09:36:02 +0200

    Fix: Blank line at end of file (pycodestyle)

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -11,4 +11,3 @@ wait_random = __import__("0-basic_async_syntax").wait_random
 def task_wait_random(max_delay: int) -> asyncio.Task:
     """Return an asyncio Task wrapping wait_random."""
     return asyncio.create_task(wait_random(max_delay))
-
